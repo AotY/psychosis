99d88575e2858194f156d9845ef3aea59c0d73b4
AotY <694731929@qq.com>
2018-01-26 14:45:56 +0800

    modify pages

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 from app import views
 
 urlpatterns = [
-    # url(r'^$',  views.index),
-    url(r'^$',  views.welcome),
+    url(r'^$',  views.index),
+    # url(r'^$',  views.welcome),
     url(r'^welcome', views.welcome), # 欢迎页面，介绍目的，感谢团队
     url(r'^index',  views.index), # index 页面
     url(r'^autism$', views.autism), # 孤独症；儿童孤独症；孤独性
